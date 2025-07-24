cd401d1f5bb952c3ac7a5ca55653381da2326ca1
a.cherkashin <a.cherkashin@notamedia.com.ru>
2025-07-25 02:00:26 +0300

    feat: section 6

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,15 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astro-blog-tutorial-site.netlify.app',
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [preact()],
 });
